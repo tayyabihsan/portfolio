c47d803514a8cff873659baa20a9da98c780a270
Tayyab Ihsan <tayyab.ihsan.dm@gmail.com>
2024-11-29 22:58:35 +0500

    updated project

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,16 @@ module.exports = {
         light: '#E4E4E4'
       },
       fontSize: {
-        '2xl': 'clamp(50px, 5vw, 65px)',
-        xl: 'clamp(45px, 5vw, 56px)',
+        '2xl': 'clamp(45px, 5vw, 65px)',
+        xl: 'clamp(40px, 5vw, 56px)',
         lg: '30px',
-        md: 'clamp(24px, 5vw, 28px)',
+        md: 'clamp(20px, 5vw, 28px)',
         sm: '20px',
         xs: '18px',
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
-        futura: ['Futura Bk BT'],
+        raleway: ['Raleway, sans-serif'],
         opensans: [ 'Open Sans, sans-serif'],
         
       }
